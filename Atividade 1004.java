2fa311d2b1b66d0dfc0b1ff0dec17e3664e04958
EddyBorges <63222336+EddyBorges@users.noreply.github.com>
2020-04-06 12:48:26 -0300

    Atividade 1004
    Código da Atividade 1004 - Produto Simples, referente a Aula 3.

---
+++ Atividade 1004.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+        
+        
+        int A;
+        int B;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o valor de A: ");
+        A = sc.nextInt();
+        
+        System.out.println("Digite o valor de B: ");
+        B = sc.nextInt();
+        
+        
+        System.out.println("PROD = " + A*B);
+        
+      
+    }
+}
